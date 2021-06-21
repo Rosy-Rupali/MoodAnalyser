46c432ff262b1913e2d7f754ac3e0a5930b6a033
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-21 15:51:31 +0530

    added: welcome message

---
+++ main/java/com/bridgelabz/moodanalyse/MoodAnalyser.java
@@ -0,0 +1,15 @@
+/************************************************************
+ * Purpose : Mood Analyser using Custom Exception 
+ * @author Rosy Rupali
+ * @Version 1.0
+ * @since 21-06-2021
+ *
+ ************************************************************/
+package com.bridgelabz.moodanalyse;
+
+public class MoodAnalyser {
+
+	public static void main(String args[]) {
+		System.out.println("Welcome to the mood analyser problem handled using custom exceptions ");
+	}
+}
